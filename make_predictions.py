b3d2cfde6cddd83cb379c1573ae87ad1cbca069d
PhiCtl <philippine.descourtils@epfl.ch>
2021-04-22 13:57:10 +0200

    added padding

--- make_predictions.py
+++ make_predictions.py
@@ -1,15 +1,12 @@
-from my_classes import GilNet, FlowerPoseDataset, MyTransformation, UnNormalize
+from my_classes import GilNet, RealFlowersDataset, MyTransformation, UnNormalize
 from my_functions import identity,\
                          draw,\
                          S_Loss_MSE
-
 from my_constants import IMAGENET_MEAN, IMAGENET_STD
 import torch
-import numpy as np
-
 import cv2
 from torchvision import transforms
-import imageio
+import torch.nn.functional as F
 
 IMAGENET_norm = transforms.Normalize(mean=IMAGENET_MEAN,std=IMAGENET_STD)
 unnorm = UnNormalize()
@@ -22,28 +19,12 @@ print("Evaluating on: {}".format(device))
 #=======================================
 # Define models
 
-#TODO: find which model is the correct one !!
 model_fnames = [
-    "model_20201207_203315",
-    "model_20201216_224455",
-    "model_20201219_125500_30"
+    "model_20201212_033500"
 ]
 
 # Define cofigurations for each model filename
-#TODO correct what is wrong in here
-models_config = {   "model_20201207_203315": {"model_name": "resnet34",
-                              "num_outputs": 16,
-                              "out_func": identity,
-                              "citerion": S_Loss_MSE,
-                              "normalization":(False, None),
-                              "rotation":(False,"")},
-                    "model_20201216_224455" :{"model_name": "resnet18",
-                              "num_outputs": 16,
-                              "out_func": identity,
-                              "citerion": S_Loss_MSE,
-                              "normalization":(True, IMAGENET_norm),
-                              "rotation":(False,"")},
-                    "model_20201219_125500_30":{"model_name": "resnet18",
+models_config = {   "model_20201212_033500": {"model_name": "resnet18",
                               "num_outputs": 16,
                               "out_func": identity,
                               "citerion": S_Loss_MSE,
@@ -69,33 +50,56 @@ for fname in model_fnames:
 
 transform = MyTransformation(image_transforms=[transforms.ToTensor()], rotation=False)
 
+#========================================
+# Load cropped flower picture
+# TODO: crop picture
 
 #=======================================
 # Load image for predictions
-
-filepath = 'data/im_test_2.jpeg'
-img = imageio.imread(filepath)
-image = torch.tensor(img, dtype=torch.float).transpose_(0,2).transpose_(1,2).unsqueeze_(0)
-
+dir = 'data/' #'../synthetic-flower-dataset/outputs/natural/'
+#filenames = ['nat_VG09_9.obj_002_0.png',
+             # 'nat_VG09_9.obj_002_1.png',
+             # 'nat_VG09_9.obj_002_2.png',
+             # 'nat_VG09_9.obj_002_3.png',
+             # 'nat_VG09_9.obj_002_4.png']
+filenames = ['im_test.jpeg']
+flower_dataset = RealFlowersDataset(file_names=filenames, root_dir = dir, transform=transform)
+prediction_loader = torch.utils.data.DataLoader(flower_dataset)
+data_loaders = {"prediction": prediction_loader}
 #=======================================
 # Compute prediction
 
-for fname in model_fnames:
-    model = models[fname]
-    image_ = image.detach().clone()
-    (apply_norm, norm) = models_config[fname]["normalization"]
-    if apply_norm:
-        image_ = torch.unsqueeze(norm(image_.squeeze()), 0)
-    else:
-        image_.mul_(255)
+for idx,data in enumerate(data_loaders["prediction"]):
 
-    output_2d = model(image_)
-    print(output_2d)
-    output = output_2d.t().reshape(8,2)
-    img_bbox = draw(img, output, black=True, ID=True)
-    cv2.imshow('result', img_bbox) #TODO : convert to bgr convention
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    image = data['image']
+   #TODO handle padding
+    d1 = int((600 - image.shape[-1])/2)
+    d2 = int((600 - image.shape[-2])/2)
+    image = F.pad(image, (d2,d2,d1,d1), "constant",0)
+    print(image.shape)
+    image = image.to(device)
 
+    for fname in model_fnames:
+        model = models[fname]
+        image_ = image.detach().clone()
+        (apply_norm, norm) = models_config[fname]["normalization"]
+        if apply_norm:
+            image_ = torch.unsqueeze(norm(image_.squeeze()), 0)
+        else:
+            image_.mul_(255)
 
+        output_2d = model(image_)
+        img = image_.squeeze().transpose(0,2).transpose(0,1).detach().numpy()
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+        print(output_2d, image.shape, img.shape)
+        output = output_2d.t().reshape(8,2)
+
+        img_bbox = draw(img, output, black=True, ID=True)
+        cv2.imshow('result', img_bbox) #TODO : convert to bgr convention
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+#=====================================
+# Get camera intrinsics and return orientation
+#=====================================
 
